fb095e90dc52c1b5579b754ce2bbd08e94aa4742
Mouhamed sow <mouhopap@gmail.com>
2025-11-10 17:55:57 +0000

    Delete

--- src/data/cars.ts
+++
@@ -1,2 +0,0 @@
-// Ce fichier est maintenant vide car les données des voitures sont gérées via Firebase Firestore.
-// Les voitures seront ajoutées et récupérées via les fonctions dans src/data/car-management.ts.
